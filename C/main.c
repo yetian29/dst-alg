d91887ab52e4f610bb9255e87e67e2896778bb09
yetian29 <you@example.comr>
2024-12-31 20:41:52 +0700

    Initital

---
+++ C/main.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct HashNode
+{
+	int key;
+	int index;
+	struct HashNode *next;
+} HashNode;
+
+typedef struct 
+{
+	HashNode **buckets;
+	int size;
+} HashTable;
+
+HashTable *createHashTable(int size)
+{
+  HashTable *table = (HashTable *)malloc(sizeof(*table));
+	table -> buckets = (HashNode **)calloc(size, sizeof(HashNode*));
+	table -> size = size;
+	return table;
+}
+
+unsigned int hash(int key, unsigned int size)
+{
+	unsigned int hash_index = abs(key) % size;
+	return hash_index;
+}
+
+HashNode *find(HashTable *table, int key)
+{
+	unsigned int index = hash(key, table -> size);
+	HashNode *node_current = table -> buckets[index];
+	while (node_current != NULL)
+	{
+		if(node_current -> key == key) return node_current;
+		node_current = node_current -> next;
+	}
+	return NULL;
+
+
+}
+HashNode *push(HashTable *table, int key, int index)
+{
+	unsigned int bucket_index = hash(key, table -> size);
+	HashNode *new_node = (HashNode *)malloc(sizeof(*new_node));
+	new_node -> key = key;
+	new_node -> index = index;
+	new_node -> next = table -> buckets[bucket_index];
+	table -> buckets[bucket_index] = new_node;
+	return new_node;
+
+}
+
+void freeHashTable(HashTable *table)
+{
+	for (int i = 0; i < table -> size; i++)
+	{
+		HashNode *current_node = table -> buckets[i];
+		while (current_node != NULL)
+		{
+			current_node = current_node -> next;
+			free(current_node);
+		}
+	}
+	free(table -> buckets);
+	free(table);
+}
+
+int main(void)
+{
+	int nums[] = {2, 3, 4, 5, 6, 7};
+	HashTable *table = createHashTable(size);
+	int target = 7;
+	HashNode *found = find(table, target);
+	freeHashTable(table);
+	return 0;
+}
