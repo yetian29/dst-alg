bf28a0def8343864921ceddb90e45503558edab0
yetian29 <you@example.comr>
2025-01-02 07:45:47 +0700

    Initital

--- Python/twoSum.py
+++ Python/twoSum.py
@@ -5,14 +5,16 @@ def twoSum(nums: list[int], target: int) -> list[int]:
     for i, number in enumerate(nums):
         complement = target - number
         if complement in number_index:
-            return [number_index[complement], i]
+            result.append(number_index[complement])
+            result.append(i)
+            return result
         number_index[number] = i
     return result
 
 
 def main():
-    nums = [2, 3, 4, 5, 6, 7]
-    target = 9
+    nums = [2, 3, 4, 5, 6]
+    target = 11
     result = twoSum(nums, target)
     print("Result:", result)
 
