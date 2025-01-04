e904ef0f1631e7348d77586d3bc0b91e9919ea7c
yetian29 <you@example.comr>
2025-01-04 09:53:33 +0700

    Initital

--- Python/linkList.py
+++ Python/linkList.py
@@ -8,12 +8,30 @@ class LinkList:
     def __init__(self):
         self.head = ListNode()
 
-    def add(self, value: int):
+    def create_node_new(self, value: int):
         new_node = ListNode(value)
+        return new_node
+
+    def add(self, value: int):
+        new_node = self.create_node_new(value)
         node_current = self.head
         while (node_current.next):
             node_current = node_current.next
         node_current.next = new_node
+
+    def insert(self, value: int, position: int):
+        new_node = self.create_node_new(value)
+        node_current = self.head
+        i = 0
+        while (node_current and i < position - 1):
+            node_current = node_current.next
+            i += 1
+        tmp =  node_current.next
+        node_current.next = new_node
+        new_node.next = tmp
+    
+
+
 
     def out(self):
         node_current = self.head
@@ -32,7 +50,11 @@ def main():
     linkList.add(3)
     linkList.add(4)
     linkList.add(5)
+    linkList.insert(6, 1)
+    linkList.insert(7, 2)
+    linkList.insert(8, 8)
     linkList.out()
+
 
 if __name__ == "__main__":
     main()
