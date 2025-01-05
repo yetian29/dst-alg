c6a980ca57bcf164cc528ee66ac980cc418aaa2e
yetian29 <you@example.comr>
2025-01-05 15:15:14 +0700

    Initital

--- C/linkList.c
+++ C/linkList.c
@@ -1,8 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct Node
 {
 	int data;
 	struct Node *next;
-} Node
+} Node;
 
 typedef struct
 {
@@ -10,13 +13,59 @@ typedef struct
 	unsigned int size;
 } LinkList;
 
+
 LinkList *createLinkList()
 {
+	LinkList *list = (LinkList *)malloc(sizeof(LinkList));
+	Node *head = (Node *)malloc(sizeof(head));
+	list -> head = head;
+	list -> size = 0;
+	return list;
+}
 
+Node *create_new_node(int data)
+{
+	Node *new_node = (Node *)malloc(sizeof(Node));
+	new_node -> data = data;
+	new_node -> next = NULL;
+	return new_node;
+}
+
+void add(LinkList *list, int value)
+{
+	Node *new_node = create_new_node(value);
+	Node *node_current = list -> head;
+	while (node_current -> next != NULL)
+	{
+			node_current = node_current -> next;
+	}
+	node_current -> next = new_node;
+	list -> size += 1;
+}
+
+void out(LinkList *list)
+{
+	Node *node_current = list -> head;
+	printf("ptr node_current: %p\n", node_current);
+	while (node_current -> next != NULL)
+	{
+		printf("%d", (node_current -> next) -> data);
+		printf(" -> ");
+		printf("ptr node current next: %p\n", node_current -> next);
+		node_current = node_current -> next;
+
+	}
+	printf("\n");
 }
 
 int main()
 {
 	LinkList *list = createLinkList();
+	add(list, 1);
+	add(list, 2);
+	add(list, 3);
+	add(list, 4);
+	add(list, 5);
+	out(list);
 	return 0;
 }
