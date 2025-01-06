75d85c93807141029411d00bc6cfa03941cb1961
yetian29 <you@example.comr>
2025-01-06 19:06:07 +0700

    Initital

--- Python/twoSum.py
+++ Python/twoSum.py
@@ -1,4 +1,28 @@
 
+# def twoSum(nums: list[int], target: int) -> list[int]:
+#     result = []
+#     number_index = {}
+#     for i, number in enumerate(nums):
+#         complement = target - number
+#         if complement in number_index:
+#             result.append(number_index[complement])
+#             result.append(i)
+#             return result
+#         number_index[number] = i
+#     return result
+#
+#
+# def main():
+#     nums = [2, 3, 4, 5, 6]
+#     target = 11
+#     result = twoSum(nums, target)
+#     print("Result:", result)
+#
+# if __name__ == "__main__":
+#     main()
+#
+
+# Return indices of two number such that sum that two number equal target
 def twoSum(nums: list[int], target: int) -> list[int]:
     result = []
     number_index = {}
@@ -13,11 +37,11 @@ def twoSum(nums: list[int], target: int) -> list[int]:
 
 
 def main():
-    nums = [2, 3, 4, 5, 6]
-    target = 11
+    nums = [2, 4, 6, 8, 1];
+    target = 12
     result = twoSum(nums, target)
     print("Result:", result)
 
+
 if __name__ == "__main__":
     main()
-
