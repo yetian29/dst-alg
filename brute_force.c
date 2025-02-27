f28517e325e348c64d475eefd089f977a72cdcf7
yetian29 <you@example.comr>
2025-02-27 20:50:46 +0700

    Initital

---
+++ brute_force.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+#define INT_MIN -2^31
+
+int main(void)
+{
+	int array[] = {1, 2, 3, 4};
+	int size = sizeof(array) / sizeof(array[0]);
+	int maxSum = INT_MIN;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i; j < size; j++)
+		{
+			int sum = 0;
+			for (int k = i; k <= j; k++)
+			{
+				sum += array[k];
+			}
+			if (sum >= maxSum)
+			{
+				maxSum = sum;
+			}
+
+		}
+
+	}
+	printf("MaxSum: %d\n", maxSum);
+
+	return 0;
+}
