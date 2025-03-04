edea6a3ce1d5a24d6bd0e97333f1d4568cbb9632
yetian29 <you@example.comr>
2025-03-04 11:18:30 +0700

    Initital

--- brute_force.c
+++ brute_force.c
@@ -1,69 +1,6 @@
-/*#include <stdio.h>*/
-/**/
-/**/
-/*#define INT_MIN -2147483648*/
-/**/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {1, 2, 3, 4};*/
-/*	int size = sizeof(array) / sizeof(array[0]);*/
-/*	int maxSum = INT_MIN;*/
-/*	for (int i = 0; i < size; i++)*/
-/*	{*/
-/*		for (int j = i; j < size; j++)*/
-/*		{*/
-/*			int sum = 0;*/
-/*			for (int k = i; k <= j; k++)*/
-/*			{*/
-/*				sum += array[k];*/
-/*			if (sum >= maxSum)*/
-/*			{*/
-/*				maxSum = sum;*/
-/*			}*/
-/*			}*/
-/**/
-/*		}*/
-/**/
-/*	}*/
-/*	printf("MaxSum: %i\n", maxSum);*/
-/**/
-/*	return 0;*/
-/*}*/
-
-
 #include <stdio.h>
-#include <time.h>
+#include <limits.h>
 
-/*#define INT_MIN -2147483648*/
-/**/
-/**/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {-2, 11, -4, 13, -5, 2};*/
-/*	int size = sizeof(array) / sizeof(array[0]);*/
-/*	int maxSum = INT_MIN;*/
-/*	clock_t startT = clock();*/
-/*	for (int i = 0; i < size; i++)*/
-/*	{*/
-/*		int sum = 0;*/
-/*		for (int j = i; j < size; j++)*/
-/*		{*/
-/*			sum += array[j];*/
-/*			if (sum > maxSum)*/
-/*			{*/
-/*				maxSum = sum;*/
-/*			}*/
-/**/
-/*		}		*/
-/*	}*/
-/*	clock_t endT = clock();*/
-/*	printf("Process time brute_force algorithm: %f\n", (double) (endT - startT) / CLOCKS_PER_SEC);	*/
-/*	printf("MaxSum: %i\n", maxSum);*/
-/*	return 0;*/
-/*}*/
-
-#include <stdio.h>
-#define INT_MIN -2147483648
 
 int MaxSum(int array[], int n)
 {
@@ -79,16 +16,16 @@ int MaxSum(int array[], int n)
 				maxSum = sum;
 			}
 		}
-
 	}
 	return maxSum;
 }
+
 int main(void)
 {
-
-	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int array[] = {1, -2, 3, 4, -5, 6};
 	int n = sizeof(array) / sizeof(array[0]);
 	int maxSum = MaxSum(array, n);
-	printf("maxSum: %i\n", maxSum);
+	printf("maxSum: %d\n", maxSum);
 	return 0;
+
 }
--- recursive.c
+++ recursive.c
@@ -1,160 +1,36 @@
-/*#include <stdio.h>*/
-/*#include <time.h>*/
-/**/
-/*#define INT_MIN -2147483648*/
-/**/
-/*int MaxLeft(int array[], int low, int high)*/
-/*{*/
-/*	int maxSum = INT_MIN;*/
-/*	int sum = 0;*/
-/*	for (int j = high; low <= j; j--)*/
-/*	{*/
-/*		sum += array[j];			*/
-/*		if (sum > maxSum)*/
-/*		{*/
-/*			maxSum = sum;*/
-/*		}*/
-/*	}*/
-/**/
-/**/
-/*	return maxSum;*/
-/*}*/
-/**/
-/*int MaxRight(int array[], int low, int high)*/
-/*{*/
-/*	int maxSum = INT_MIN;*/
-/*	int sum = 0;*/
-/*	for (int j = low; j <= high; j++)*/
-/*	{*/
-/*		sum += array[j];			*/
-/*		if (sum > maxSum)*/
-/*		{*/
-/*			maxSum = sum;*/
-/*		}*/
-/*	}*/
-/*	return maxSum;*/
-/*}*/
-/**/
-/*int Max(int wl, int wm, int wr)*/
-/*{*/
-/*  int max = wl;*/
-/*	if (wm > max)*/
-/*	{*/
-/*		max = wm;*/
-/*	}*/
-/*	if (wr > max)*/
-/*	{*/
-/*		max = wr;*/
-/*	}*/
-/*	return max;*/
-/*}*/
-/**/
-/*int MaxSum(int array[], int low, int high)*/
-/*{*/
-/*	if (low == high) return array[low];*/
-/*	int mid = (low + high) / 2;*/
-/*	int wl = MaxSum(array, low, mid);*/
-/*	int wr = MaxSum(array, mid + 1, high);*/
-/*	int wm = MaxLeft(array, low, mid) + MaxRight(array, mid + 1, high);*/
-/*	return Max(wl, wm, wr);*/
-/*}*/
-/**/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {-2, 11, -4, 13, -5, 2};*/
-/*	int size = sizeof(array) / sizeof(array[0]);*/
-/*	clock_t startT = clock();*/
-/*	int maxSum = MaxSum(array, 0, size - 1);*/
-/*	clock_t endT = clock();*/
-/*	printf("Process time divide and conquer algorithms: %f\n", (double) (endT - startT) / CLOCKS_PER_SEC);*/
-/*	printf("MaxSum: %i\n", maxSum);*/
-/*	return 0;*/
-/*}*/
-
-
-/*#include <stdio.h>*/
-/**/
-/*int calculate(int n)*/
-/*{*/
-/*	if (n == 1) return 1;*/
-/*	return n * calculate(n - 1);*/
-/*}*/
-/**/
-/*int main(void)*/
-/*{*/
-/*	// Computing n!*/
-/*	int n;*/
-/*	printf("Enter n: ");*/
-/*	scanf("%i", &n);*/
-/*	int result = calculate(n);*/
-/*	printf("Result: %i\n", result);*/
-/*	return 0;*/
-/*}*/
-
-
-/*#include <stdio.h>*/
-/*#include <stdbool.h>*/
-/**/
-/*bool BinarySearch(int array[], int low, int high, int key)*/
-/*{	*/
-/*	if (low == high) return array[low] == key;*/
-/*	int mid = low + (high - low) / 2;*/
-/*	if (key == array[mid]) return true;	*/
-/*	else if (key < array[mid]) return BinarySearch(array, low, mid - 1, key);*/
-/*	else return BinarySearch(array, mid + 1, high, key);*/
-/*}*/
-/**/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {1, 2, 3, 4, 5, 6};*/
-/*	int key = 5;*/
-/*	int size = sizeof(array) / sizeof(array[0]);*/
-/*	bool result = BinarySearch(array, 0, size - 1, key);*/
-/*	printf("%s\n", result ? "Found" : "Not Found");*/
-/*	return 0;*/
-/*}*/
-/**/
-
 #include <stdio.h>
-#define INT_MIN -2147483648
-
+#include <limits.h>
 
 int MaxLeft(int array[], int low, int high)
 {
-	int sum = 0;
 	int maxSum = INT_MIN;
-	for (int i = high; low <= i; i--)
+	int sum =  0;
+	for (int i = high; i >= low; i--)
 	{
 		sum += array[i];
-		if (sum > maxSum)
-		{
-			maxSum = sum;
-		}
+		if (sum > maxSum) maxSum = sum;
 	}
 	return maxSum;
 }
 
 int MaxRight(int array[], int low, int high)
 {
-	int sum = 0;
 	int maxSum = INT_MIN;
+	int sum =  0;
 	for (int i = low; i <= high; i++)
 	{
 		sum += array[i];
-		if (sum > maxSum)
-		{
-			maxSum = sum;
-		}
+		if (sum > maxSum) maxSum = sum;
 	}
 	return maxSum;
 }
 
-int Max(int wl, int wm, int wr)
+int Max(int v1, int v2, int v3)
 {
-	int max = wl;
-	if (wm > max) max = wm;
-	if (wr > max) max = wr;
-	return max;
+	int maxValue = v1;
+	if (v2 > maxValue) maxValue = v2;
+	if (v3 > maxValue) maxValue = v3;
+	return maxValue;
 }
 
 int MaxSum(int array[], int low, int high)
@@ -162,18 +38,17 @@ int MaxSum(int array[], int low, int high)
 	if (low == high) return array[low];
 	int mid = low + (high - low) / 2;
 	int wl = MaxSum(array, low, mid);
-	int wm = MaxLeft(array, low, mid) + MaxRight(array, mid + 1, high);
 	int wr = MaxSum(array, mid + 1, high);
-	return Max(wl, wm, wr);
-}
+	int wm = MaxRight(array, mid + 1, high) + MaxLeft(array, low, mid);
+	return Max(wl, wr, wm);
 
+}
 
 int main(void)
 {
-	int array[] = {1, -2, 4, -10, 3, 5, -100};
+	int array[] = {1, -2, 3, 4, -5, 6};
 	int n = sizeof(array) / sizeof(array[0]);
 	int maxSum = MaxSum(array, 0, n - 1);
 	printf("maxSum: %d\n", maxSum);
 	return 0;
 }
-
