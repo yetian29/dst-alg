4e5b8ea6c132d2734076674a45f6df6293d9cb28
yetian29 <you@example.comr>
2025-03-06 16:45:25 +0700

    Initital

--- prod.c
+++ prod.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 
 
-int HelpProd(int array[], int k, int n)
+int n;
+
+int HelpProd(int array[], int k)
 {
 	if (k == n - 1) return array[k];
-	return HelpProd(array, k + 1, n) * array[k];
+	return HelpProd(array, k + 1) * array[k];
 }
 
 int main(void)
 {
 	int array[] = {1, 2, 3, 4, 5, 6, 7};
-	int n = sizeof(array) / sizeof(array[0]);
-	int result = HelpProd(array, 0, n);
+	n = sizeof(array) / sizeof(array[0]);
+	int result = HelpProd(array, 0);
 	printf("%d\n", result);
 	return 0;
 }
