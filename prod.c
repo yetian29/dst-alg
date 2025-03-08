9ab4ee2b04c65c221944c4889b590859bae91739
yetian29 <you@example.comr>
2025-03-08 20:08:22 +0700

    Initital

--- prod.c
+++ prod.c
@@ -1,20 +1,35 @@
 
+/*#include <stdio.h>*/
+/**/
+/**/
+/*int n;*/
+/**/
+/*int HelpProd(int array[], int k)*/
+/*{*/
+/*	if (k == n - 1) return array[k];*/
+/*	return HelpProd(array, k + 1) * array[k];*/
+/*}*/
+/**/
+/*int main(void)*/
+/*{*/
+/*	int array[] = {1, 2, 3, 4, 5, 6, 7};*/
+/*	n = sizeof(array) / sizeof(array[0]);*/
+/*	int result = HelpProd(array, 0);*/
+/*	printf("%d\n", result);*/
+/*	return 0;*/
+/*}*/
 #include <stdio.h>
 
-
-int n;
-
-int HelpProd(int array[], int k)
+int HelpProd(int array[], int n)
 {
-	if (k == n - 1) return array[k];
-	return HelpProd(array, k + 1) * array[k];
+	if (n == 1) return array[0];
+	return HelpProd(array, n - 1) * array[n - 1];
 }
 
 int main(void)
 {
 	int array[] = {1, 2, 3, 4, 5, 6, 7};
-	n = sizeof(array) / sizeof(array[0]);
-	int result = HelpProd(array, 0);
-	printf("%d\n", result);
-	return 0;
+	int n = sizeof(array) / sizeof(array[0]);
+	int result = HelpProd(array, n);
+	printf("Result: %d\n", result);
 }
--- sum.c
+++ sum.c
@@ -1,16 +1,33 @@
 
+/*#include <stdio.h>*/
+/**/
+/*int Sum(int array[], int low, int high)*/
+/*{*/
+/*	if (low == high) return array[low];*/
+/*	return Sum(array, low, high - 1) + array[high];*/
+/*}*/
+/**/
+/*int main(void)*/
+/*{*/
+/*	int array[] = {1, 2, 3, 4, 5, 6, 7};*/
+/*	int n = sizeof(array) / sizeof(array[0]);*/
+/*	int result = Sum(array, 0, n - 1);*/
+/*	printf("Result: %d\n", result);*/
+/*}*/
+
 #include <stdio.h>
 
-int Sum(int array[], int low, int high)
+int sum(int array[], int n)
 {
-	if (low == high) return array[low];
-	return Sum(array, low, high - 1) + array[high];
+	if (n == 1) return array[0];
+	return sum(array, n - 1) + array[n - 1];
 }
 
 int main(void)
 {
-	int array[] = {1, 2, 3, 4, 5, 6, 7};
+	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int n = sizeof(array) / sizeof(array[0]);
-	int result = Sum(array, 0, n - 1);
+	int result = sum(array, n);
 	printf("Result: %d\n", result);
+	return 0;
 }
