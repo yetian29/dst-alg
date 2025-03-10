13b572f6adeda43a347690065e26a61edd47357c
yetian29 <you@example.comr>
2025-03-10 20:46:24 +0700

    Initial

--- maxSum.c
+++ maxSum.c
@@ -1,124 +1,10 @@
-/*#include <stdio.h>*/
-/*#include <limits.h>*/
-/**/
-/**/
-/*int BruteForce(int array[], int n)*/
-/*{*/
-/*	int maxSum = INT_MIN;*/
-/*	for (int i = 0; i < n; i++)*/
-/*	{*/
-/*		int sum = 0;*/
-/*		for (int j = i; j < n; j++)*/
-/*		{*/
-/*			sum += array[j];*/
-/*			if (sum > maxSum)*/
-/*			{*/
-/*				maxSum = sum;*/
-/*			}*/
-/*		}*/
-/*	}*/
-/*	return maxSum;*/
-/*}*/
-/**/
-/*int MaxLeft(int array[], int low, int high)*/
-/*{*/
-/*	int maxSum = INT_MIN;*/
-/*	int sum = 0;*/
-/*	for (int i = high; low <= i; i--)*/
-/*	{*/
-/*		sum += array[i];*/
-/*		if (sum > maxSum)*/
-/*		{*/
-/*			maxSum = sum;*/
-/*		}*/
-/*	}*/
-/*	return maxSum;*/
-/*}*/
-/*int MaxRight(int array[], int low, int high)*/
-/*{*/
-/*	int maxSum = INT_MIN;*/
-/*	int sum = 0;*/
-/*	for (int i = low; i <= high; i++)*/
-/*	{*/
-/*		sum += array[i];*/
-/*		if (sum > maxSum)*/
-/*		{*/
-/*			maxSum = sum;*/
-/*		}*/
-/*	}*/
-/*	return maxSum;*/
-/*}*/
-/**/
-/*int Max1(int v1, int v2, int v3)*/
-/*{*/
-/*	int maxValue = v1;*/
-/*	if (v2 > maxValue)*/
-/*	{*/
-/*		maxValue = v2;*/
-/*	}*/
-/*	if (v3 > maxValue)*/
-/*	{*/
-/*		maxValue = v3;*/
-/*	}*/
-/*	return maxValue;*/
-/*}*/
-/**/
-/*int DivideAndConquer(int array[], int low, int high)*/
-/*{*/
-/*	if (low == high)*/
-/*	{*/
-/*		return array[low];*/
-/*	}*/
-/*	int mid = low + (high - low) / 2;*/
-/*	int wl = DivideAndConquer(array, low, mid);*/
-/*	int wr = DivideAndConquer(array, mid + 1, high);*/
-/*	int wm = MaxLeft(array, low, mid) + MaxRight(array, mid + 1, high);*/
-/*	return Max1(wl, wr, wm);*/
-/**/
-/*}*/
-/**/
-/*int Max2(int v1, int v2)*/
-/*{*/
-/*	int maxValue = v1;*/
-/*	if (v2 > maxValue)*/
-/*	{*/
-/*		maxValue = v2;*/
-/*	}*/
-/*	return maxValue;*/
-/*}*/
-/**/
-/*int Kadane(int array[], int n)*/
-/*{*/
-/*	// declare si is weight max we need to find, ei is weight max contain last element and end at the element*/
-/**/
-/*	int ei = array[0];*/
-/*	int si = array[0];*/
-/*	for (int i = 1; i < n; i++)*/
-/*	{*/
-/*		ei = Max2(array[i], array[i] + ei);*/
-/*		si = Max2(si, ei);*/
-/*	}*/
-/*	return si;*/
-/**/
-/*}*/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {1, -2, 3, -4, 5, -6};*/
-/*	int n = sizeof(array) / sizeof(array[0]);*/
-/*	/*int maxSum = BruteForce(array, n);*/
-/*	/*int maxSum = DivideAndConquer(array, 0, n - 1);*/
-/*	int maxSum = Kadane(array, n);*/
-/*	printf("maxSum: %d\n", maxSum);*/
-/*	return 0;*/
-/*}*/
-
 #include <stdio.h>
 #include <limits.h>
 
 int BruteForce(int array[], int n)
 {
 	int maxSum = INT_MIN;
-	for (int i = 0; i < n; i++)
+	for (int i  = 0; i < n; i++)
 	{
 		int sum = 0;
 		for (int j = i; j < n; j++)
@@ -128,7 +14,6 @@ int BruteForce(int array[], int n)
 			{
 				maxSum = sum;
 			}
-
 		}
 	}
 	return maxSum;
@@ -145,10 +30,10 @@ int MaxLeft(int array[], int low, int high)
 		{
 			maxSum = sum;
 		}
-
 	}
 	return maxSum;
 }
+
 int MaxRight(int array[], int low, int high)
 {
 	int sum = 0;
@@ -160,29 +45,22 @@ int MaxRight(int array[], int low, int high)
 		{
 			maxSum = sum;
 		}
-
 	}
 	return maxSum;
 }
+
 int Max(int value1, int value2, int value3)
 {
 	int max = value1;
-	if (value2 > max)
-	{
-		max = value2;
-	}
-	if (value3 > max)
-	{
-		max = value3;
-	}
+	if (value2 > max) max = value2;
+	if (value3 > max) max = value3;
 	return max;
 }
-
 
 int DivideAndConquer(int array[], int low, int high)
 {
 	if (low == high) return array[low];
-	int mid = low + (high - low) / 2;
+	int mid = low + (high  - low) / 2;
 	int wl = DivideAndConquer(array, low, mid);
 	int wm = MaxLeft(array, low, mid) + MaxRight(array, mid + 1, high);
 	int wr = DivideAndConquer(array, mid + 1, high);
@@ -192,32 +70,32 @@ int DivideAndConquer(int array[], int low, int high)
 int Max2(int value1, int value2)
 {
 	int max = value1;
-	if (value2 > max)
-	{
-		max = value2;
-	}
+	if (value2 > max) max = value2;
 	return max;
 }
 
-int DP(int array[], int n)
+int DynamicProgramming(int array[], int n)
 {
-	int ei = array[0];
 	int si = array[0];
+	int ei = array[0];
+
 	for (int i = 1; i < n; i++)
 	{
 		ei = Max2(array[i], array[i] + ei);
 		si = Max2(si, ei);
 	}
 	return si;
+
 }
+
 
 int main(void)
 {
-	int array[] = {1, -2, 3, -4, 5, -6};
-	int n = sizeof(array) / sizeof(array[0]);
-	/*int result = BruteForce(array, n);*/
-	/*int result = DivideAndConquer(array, 0, n - 1);*/
-	int result = DP(array, n);
-	printf("Result: %d\n", result);
+	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int size = sizeof(array) / sizeof(array[0]);
+	/*printf("maxSum: %d\n", BruteForce(array, size));*/
+	/*printf("maxSum: %d\n", DivideAndConquer(array, 0, size - 1));*/
+	printf("maxSum: %d\n", DynamicProgramming(array, size));
+
 	return 0;
 }
--- sum.c
+++ sum.c
@@ -1,33 +1,15 @@
-
-/*#include <stdio.h>*/
-/**/
-/*int Sum(int array[], int low, int high)*/
-/*{*/
-/*	if (low == high) return array[low];*/
-/*	return Sum(array, low, high - 1) + array[high];*/
-/*}*/
-/**/
-/*int main(void)*/
-/*{*/
-/*	int array[] = {1, 2, 3, 4, 5, 6, 7};*/
-/*	int n = sizeof(array) / sizeof(array[0]);*/
-/*	int result = Sum(array, 0, n - 1);*/
-/*	printf("Result: %d\n", result);*/
-/*}*/
-
 #include <stdio.h>
 
-int sum(int array[], int n)
+int Sum(int array[], int n)
 {
 	if (n == 1) return array[0];
-	return sum(array, n - 1) + array[n - 1];
+	return Sum(array, n - 1) + array[n - 1];
 }
 
 int main(void)
 {
 	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int n = sizeof(array) / sizeof(array[0]);
-	int result = sum(array, n);
-	printf("Result: %d\n", result);
+	int size = sizeof(array) / sizeof(array[0]);
+	printf("Sum: %d\n", Sum(array, size));
 	return 0;
 }
