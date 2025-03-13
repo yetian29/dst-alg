faa4e1f524646d519e7b5ec893743009c650ec96
yetian29 <you@example.comr>
2025-03-13 11:01:51 +0700

    Initial

--- fibonnaci.c
+++ fibonnaci.c
@@ -3,25 +3,25 @@
 
 int Recursive(int n)
 {
-	if (n == 1 || n == 2) return 1;
+	if (n < 2) return n;
 	return Recursive(n - 1) + Recursive(n - 2);
 }
 
 
 int DP(int n, int memoization[])
 {
-	if (n == 1 || n == 2) return 1;
-	if (memoization[n - 1] != -1) return memoization[n - 1];
-	memoization[n - 1] = DP(n - 1, memo) + DP(n - 2, memoization);
-	return memoization[n - 1];
+	if (n < 2) return n;
+	if (memoization[n] != -1) return memoization[n];
+	memoization[n] = DP(n - 1, memoization) + DP(n - 2, memoization);
+	return memoization[n];
 
 }
 
 int main(void)
 {
 	int n = 5;
-	int memoization[n];
-	for (int i = 0; i < n; i++) memoization[i] = -1;
+	int memoization[n + 1];
+	for (int i = 0; i < n + 1; i++) memoization[i] = -1;
 	/*printf("Result: %d\n", Recursive(n));*/
 	printf("Result: %d\n", DP(n, memoization));
 }
