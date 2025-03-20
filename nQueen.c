b553146f0a57667766fe1c1c292a0700856ea25a
yetian29 <you@example.comr>
2025-03-20 21:11:38 +0700

    Initial

--- nQueen.c
+++ nQueen.c
@@ -1,22 +1,52 @@
+#include <stdio.h>
 
-void Queen(int matrix[4][4], int row, int column)
+bool CheckClash()
 {
-	// Base case
-	// Recursive
+
 }
 
+void Queen(int matrix[4][4], int column)
+{
+	// Base case
+	if (CheckClash()) return;
+
+	// Recursive case
+	for (int row = 0; row < 4; i++)
+	{
+		matrix[row][column] = 1;
+		Queen(matrix, column + 1);
+		matrix[row][column] = -1;
+	}
+
+}
+
+void out(int matrix[4][4])
+{
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (matrix[i][j] == 1)
+			{
+				printf("(%d, %d)\n", i, j);
+			}
+		}
+	}
+}
 
 int main(void)
 {
 	int n = 4;
-	int matrix[4][4];
-	for (int i = 0; i < 4; i++)
+	int matrix[n][n];
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < 4; i++)
+		for (int j = 0; j < n; i++)
 		{
 			 matrix[i][j] = -1;
 		}
 	}
-	Queen(matrix);
+	Queen(matrix, 0);
+	out(matrix);
+
 	return 0;
 }
