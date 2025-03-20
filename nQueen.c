78970604bffc435c3da7d065c92c8f19bd06d841
yetian29 <you@example.comr>
2025-03-20 18:55:45 +0700

    Initial

---
+++ nQueen.c
@@ -0,0 +1,22 @@
+
+void Queen(int matrix[4][4], int row, int column)
+{
+	// Base case
+	// Recursive
+}
+
+
+int main(void)
+{
+	int n = 4;
+	int matrix[4][4];
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; i++)
+		{
+			 matrix[i][j] = -1;
+		}
+	}
+	Queen(matrix);
+	return 0;
+}
