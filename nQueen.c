02d9791cf6e4842904a0bccdcc11fc21c8884bd8
yetian29 <you@example.comr>
2025-03-22 11:11:24 +0700

    Initial

--- nQueen.c
+++ nQueen.c
@@ -1,37 +1,23 @@
+#include <stdio.h>
 
 int n = 4;
 
-
-bool IsSafe(int row, int column)
+void PlaceQueen(int matrix[n][n])
 {
-	if (maxtrix[row][column] == 1) return false
-	
-	return true;
-
-}
-voide PlaceQueen(int matrix[n][n], int column)
-{
-	if (column > 3) return;
-	for (int row = 0; row < n; row++)
-	{
-		if (IsSafe(row, column))
-		{
-			matrix[row][column] = 1;
-		}
-	  else PlaceQueen(matrix, column + 1)
-	}
-}
 
 }
 
 
 int main(void)
 {
-	int matrix[n][n]
+	int matrix[n][n];
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++) matrix[i][j] = -1;
+		for (int j = 0; j < n; j++)
+		{
+			matrix[i][j] = -1;
+		}
 	}
-	PlaceQueen(matrix)
+	PlaceQueen(matrix);
 	return 0;
 }
