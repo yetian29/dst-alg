b3188e8c1041f938343e6f54488dac4c290e8072
yetian29 <you@example.comr>
2025-03-27 09:07:56 +0700

    Initial

--- c/linkList.c
+++ c/linkList.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <stdlib.h>
 
+int size = 1;
 
 typedef struct Node 
 {
@@ -29,8 +30,47 @@ void addNewNodeToTail(Node **tail, int value)
 	Node *new_node = createNewNode(value);
 	(*tail)->next = new_node;
 	*tail = (*tail)->next;
+	size++;
 }
 
+void insertNewNode(Node *head, int position, int value)
+{
+	Node *ptr = head;
+	int i = 0;
+	while (ptr!= NULL && i < position - 1)
+	{
+		if (ptr->next != NULL) ptr = ptr->next;
+		i++;
+	}
+	Node *tmp = ptr->next;
+	Node *new_node = createNewNode(value);
+	ptr->next = new_node;
+	new_node->next = tmp;
+	size++;
+
+}
+
+void removalNode(Node *head, int position)
+{
+	if (position >= size)
+	{
+		fprintf(stderr, "Location delete outside range\n");
+		exit(1);	
+	}
+	Node *ptr = head;
+	int i = 0;
+	while (ptr!= NULL && i < position - 1)
+	{
+		if (ptr->next != NULL) ptr = ptr->next;
+		i++;
+	}
+	Node *deleted = ptr->next;
+	Node *after = deleted->next;
+	ptr->next = after;
+	size--;
+	free(deleted);
+
+}
 
 Node *initLinkList()
 {
@@ -78,6 +118,14 @@ int main(void)
 	addNewNodeToTail(&tail, 2);
 	addNewNodeToTail(&tail, 3);
 	addNewNodeToTail(&tail, 4);
+
+	insertNewNode(head, 1, 5);
+	insertNewNode(head, 3, 6);
+
+	removalNode(head, 3);
+	removalNode(head, 5);
+	removalNode(head, 5);
+
 
 	out(head);
 	freeLinkList(head);
